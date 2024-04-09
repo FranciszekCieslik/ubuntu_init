8751e398ab18adf0ddcea8107bcfeb32c13f7292
Franciszek Cieślik <115489360+FranciszekCieslik@users.noreply.github.com>
2024-04-09 22:56:05 +0200

    add script

---
+++ ubuntu_init.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt install -y \
+git \
+curl \
+wget \
+vim \
+htop \
+tmux \
+snapd
+
+# Install python3 and pip
+sudo apt update
+sudo apt install python3
+sudo apt install python3-pip
+sudo pip3 install --upgrade pip
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#install vscode
+sudo snap -y install --classic code
